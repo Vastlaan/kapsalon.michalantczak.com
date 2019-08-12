779ceb66d2947ebedc756d4e89b96573c343a585
vastlaan <vastlaan@mail.com>
2019-08-12 09:53:25 +0200

    initial deploy

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.post('/api/appointment', (req,res)=>{
 	res.status(200).json("Ok")
 })
 
+if(process.env.NODE_ENV==="production"){
+
+	app.use(express.static("client/build"))
+	const path = require('path')
+
+	app.get('*', (req,res)=>{		
+		res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+	})
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
