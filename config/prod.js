826f644644dc953dbc1c1bd02cbe7ea34bd089f5
vastlaan <vastlaan@mail.com>
2019-08-19 09:09:29 +0200

    adding ignored prod.js

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports ={
+	DATABASE_HOST:process.env.DATABASE_HOST,
+	DATABASE_USER:process.env.DATABASE_USER,
+	DATABASE_SECRET: process.env.DATABASE_SECRET,
+	DATABASE_NAME:process.env.DATABASE_NAME,
+	DATABASE_PORT: process.env.DATABASE_PORT
+}
